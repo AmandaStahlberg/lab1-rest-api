1ab23699b7cfa20da3882cf0a8e541d83340da60
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2021-04-09 11:32:38 +0200

    lagt till en lista med produkter, hämta en produtkt med id

--- server.js
+++ server.js
@@ -2,7 +2,47 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const products = [
+    {
+        id: 1,
+        type: 'Tröja',
+        size: 'M'
+    },
+    {
+        id: 2,
+        type: 'Byxa',
+        size: 'L'
+    },
+    {
+        id: 3,
+        type: 'Hoodie',
+        size: 'S'
+    }
+]
+
 app.use(express.static('public'))
+
+app.use(express.json())
+
+app.get("/api", (req, res) => {
+    res.json(products)
+})
+
+app.get("/api/:id", (req, res) => {
+    const id = req.params.id
+
+    const foundProduct = products.find((product) => {
+        return product.id == id
+    })
+
+    if(!foundProduct) {
+        res.json({"Error": "Tröjan du söker finns inte." })
+    }
+    
+    res.json(foundProduct)
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server is on http://localhost:${port}`)
